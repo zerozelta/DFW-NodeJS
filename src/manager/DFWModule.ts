2a78ab5116c8752ebe0e8ad3a5f9893d705491aa
Aldo César <agutierrez@rsimail.com>
2023-07-20 15:05:26 -0600

    prisma update

--- src/manager/DFWModule.ts
+++ src/manager/DFWModule.ts
@@ -1,11 +1,12 @@
 import { PrismaClient } from "@prisma/client";
+import * as runtime from '@prisma/client/runtime/library';
 import { NextFunction } from "express";
 import DFWInstance from "../DFWInstance";
 
 export default abstract class DFWModule {
 
     private _instance: DFWInstance;
-    private _db: PrismaClient|Omit<PrismaClient, '$connect' | '$disconnect' | '$on' | '$transaction' | '$use'>;
+    private _db: Omit<PrismaClient, runtime.ITXClientDenyList>;
 
     constructor(DFW: DFWInstance) {
         this._instance = DFW;
@@ -20,7 +21,7 @@ export default abstract class DFWModule {
         return this._instance;
     }
 
-    public use(db:PrismaClient|Omit<PrismaClient, '$connect' | '$disconnect' | '$on' | '$transaction' | '$use'>): this {
+    public use(db:PrismaClient|Omit<PrismaClient, runtime.ITXClientDenyList>): this {
         this._db = db;
         return this;
     }
